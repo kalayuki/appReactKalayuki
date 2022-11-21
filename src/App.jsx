3da5ab920899a08c68a4be4d0991e8a8d1e2c15d
kalayuki <kalayuki.emilia@gmail.com>
2022-11-21 19:45:06 -0300

    clase 1, 2 ,3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './componets/Footer';
+import Saludo from './componets/Saludo';
+
+
 
 function App() {
+  //Establecems una constante para poder observar como lo establecemos en el contenido
+  const boton= <button>BOTON</button>
+  const nombre='Mauricio'
+  //Agregamos un estilo
+  const estilo ={
+    color:'red',  
+    backgroundColor:'yellow',
+    padding:20, 
+  }
+
+  const mensaje =()=>{
+    console.log('mensaje de la consola')
+  } 
+  
   return (
     <div className="App">
       <h1> Hola Mundo</h1>
+      <strong style={estilo}>Este es un mensaje para {nombre} </strong>
+      {boton}
+
+
+      <Saludo name='Gabriel' age='31' lastName='Kalayuki' action={mensaje}></Saludo> 
+      <Saludo name='ana' lastName='Perez' age='34' action={mensaje}></Saludo>
+
+      <li>Fresa</li>
+      <li>Melon</li>
+      <li>Sandia</li>
+      <li>Berrys</li>
+      <li>Banano</li>
+      <Footer/>
     </div>
   );
 }
---
+++ src/componets/Footer.jsx
@@ -0,0 +1,10 @@
+//El impot react from , no es necesario dejarlo , se lo puede sacar
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>footer</div>
+  )
+}
+
+export default Footer
